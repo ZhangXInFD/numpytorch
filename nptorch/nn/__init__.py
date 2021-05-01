bb8f384b7fbd56885be832a4672ce7bf6a46f534
ZhangXInFD <17307130150@fudan.edu.cn>
2021-05-01 13:28:45 +0800

    2rd

--- nptorch/nn/__init__.py
+++ nptorch/nn/__init__.py
@@ -2,4 +2,5 @@ from .Sequential import *
 from .Loss import Loss
 from .functional import *
 
-__all__ = ['Sequential', 'Loss', 'Linear', 'Conv2d', 'Relu', 'Sigmoid', 'CrossEntropyLoss']
+__all__ = ['Sequential', 'Loss', 'Linear', 'Conv2d', 'Relu', 'Sigmoid', 'CrossEntropyLoss', 'MSELoss', 'MaxPool2d'
+    , 'Tanh', 'Droupout', 'Modules', 'AvgPool2d', 'Layer', 'Flatten', 'LossFunction']
